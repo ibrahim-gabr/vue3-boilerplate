3358ec569a30d5492b8f4be66462788427b4403f
ibrahim gabr <ibrahim.G@salla.sa>
2023-02-17 23:36:52 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import LogoSVG from "@/assets/logo.svg?component";
+</script>
+
+<template>
+  <header>
+    <LogoSVG alt="Vite logo" class="logo" />
+
+    <div class="wrapper bg-red-300">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+.logo {
+  @apply h-8 w-8;
+}
+</style>
